d0d9f8f2e6498e77f16c4e10d42ec48f7b457b16
Luca Michelotto <michelotto.luca@gmail.com>
2019-10-29 08:34:33 -0400

    function to add from array element now works using reduce method

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-//create variables to store the points given to each team member
+// create function that receives 2 parameters (team member name, moneyTotal) 
+// the function should return how much team member makes out of total money
+// if the name doesn't exist in array return zero
 
-// var position = ['bartender', 'captain', 'runner', 'busser'];
-// var teamMembers = ['Luca', 'Sina', 'Gaby', 'Gigi', 'Era', 'Zeus'];
-//create array of team members
 var team = [
     {
     name : 'Luca',
@@ -29,45 +28,14 @@ var team = [
     tips : 0
     },
 ];
-// create function that receives 2 parameters (team member name, moneyTotal) 
-// the function should return how much team member makes out of total money
-// if the name doesn't exist in array return zero
 
 // create a function that counts the tips
-function tipTotal (totalTips) {
-    
-    for (var i = 0; i < team.length; i++){
-        
-        var tips = team[i].tips;
-        var sum = 0;
-        sum += tips; 
-
-        return 'your total tips are' + sum;
-        }
-    }
-tipTotal();
+var tipsTotal = team.reduce(function(prev, cur) {
+    return prev + cur.tips;
+}, 0);
+console.log('your total tips are', tipsTotal);// should return 1500
 
 
 //create a function that return how much team member makes out of total money
-// function nameTot (names, totalTips) {
-//     for (var i = 0; i < team.length; i++){
-//         if ( team[i].name == team[i].tips){
-//             return team.name + team.tips }
-//                 else {
-//                     return 0;
-//                 }
-//             }
-//         }
-    
-// nameTot();
 
 
-// var totalPoints = teamMembers;
-// var moneyTotal = 1000;
-
-// function getPointValue (totalPoints, moneyTotal) {
-//     return Math.floor(totalPoints/moneyTotal*100);
-// }
-// var pointValue = getPointValue(totalPoints,moneyTotal);
-
-// tipCalc(teamName, tipTotal);
